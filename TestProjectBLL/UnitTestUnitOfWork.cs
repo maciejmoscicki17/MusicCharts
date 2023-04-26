e0b7dd999026f3e58a081e446138df73bf3e8437
mikolajbryla <mikolaj.bryla5@gmail.com>
2023-04-26 21:58:51 +0200

    dummy function exception

--- TestProjectBLL/UnitTestUnitOfWork.cs
+++ TestProjectBLL/UnitTestUnitOfWork.cs
@@ -16,11 +16,23 @@ namespace TestProjectBLL
             var piosenkaRepo = new PiosenkaRepositoryDummy();
             var chartPiosenekRepo = new ChartPiosenekRepositoryDummy();
             var context = new ListaPrzebojowContext();
-            var testUnitOfWork = new TestUnitOfWork(context,albumRepository:albumRepo);
             var unitOfWork = new UnitOfWork(context, albumRepository: albumRepo, chartPiosenek: chartPiosenekRepo, piosenkaRepository: piosenkaRepo);
             Assert.Same(albumRepo, unitOfWork.Albums);
             Assert.Same(piosenkaRepo, unitOfWork.Piosenki);
             Assert.Same(chartPiosenekRepo, unitOfWork.ChartyPiosenek);
+
+        }
+
+        [Fact]
+        public void TestDummyFunction()
+        {
+            var albumRepo = new AlbumRepositoryDummy();
+            var piosenkaRepo = new PiosenkaRepositoryDummy();
+            var chartPiosenekRepo = new ChartPiosenekRepositoryDummy();
+            var context = new ListaPrzebojowContext();
+            var unitOfWork = new UnitOfWork(context, albumRepository: albumRepo, chartPiosenek: chartPiosenekRepo, piosenkaRepository: piosenkaRepo);
+            Func<bool> func = () => albumRepo.Any(1);
+            Assert.Throws<NotImplementedException>(() => func.Invoke());
 
         }
     }
